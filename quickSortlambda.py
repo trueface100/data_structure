3e599bd80dc144d5893dd32e4fe6693dc25cbe65
trueface100 <102716297+trueface100@users.noreply.github.com>
2022-05-19 02:28:27 +0900

    Update quickSortlambda.py

--- quickSortlambda.py
+++ quickSortlambda.py
@@ -1,15 +1,17 @@
-def quickSort(A,p:int,r:int):
+def quicksortlambda(A,B,p:int,r:int):
     if p<r:
-        q=partition(A,p,r)
-        quickSort(A,p,q-1)
-        quickSort(A,q+1,r)
+        q=partitions(A,B,p,r)
+        quicksortlambda(A,B,p,q-1)
+        quicksortlambda(A,B,q+1,r)
 
-def partition(A,p:int,r:int):
-    list[A]
+def partitions(A,B,p:int,r:int):
+    x=A[r]
     i=p-1
     for j in range(p,r):
-        if A[j]<x:
+        if A[j]>x:
             i+=1
             A[i],A[j]=A[j],A[i]
+            B[i],B[j]=B[j],B[i]
     A[i+1],A[r]=A[r],A[i+1]
+    B[i+1],B[r]=B[r],B[i+1]
     return i+1
